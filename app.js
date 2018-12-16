a773c21d9e807535cc601d4faadf53430c9ba94e
Leon Salsiccia <leon.salsiccia@gmail.com>
2018-12-17 09:30:11 +1100

    fixed quality added scroll

--- app.js
+++ app.js
@@ -13,29 +13,20 @@ app.use(express.static("Movies"))
 app.use(express.static("public"))
 
 app.get("/api/", async (req, res) => {
+  console.log("Getting...");
   const movies = await Promise.all(fs.readdirSync("Movies").map(async movie => {
     if (!movie.startsWith(".")) {
       const durationSec = await videoDuration.getVideoDurationInSeconds("./movies/" + movie)
       const res = await videoResolution("./movies/" + movie)
-      let quality
-      switch (res.width) {
-        case 640:
-          quality = "480p"
-          break
-        case 1280:
-          quality = "720p"
-          break
-        case 1920:
-          quality = "1080p"
-          break
-        default:
-          quaity = "N/A"
-      }
+      let quality = res.height
+      if (res.height > 480) quality = 720;
+      if (res.height > 720 && res.width != 1280) quality = 1080;
+      
       return {
         title: movie.split("_")[0],
         genre: movie.split("_")[1].split("-").sort(),
         year: movie.split("_")[2].split(".")[0],
-        quality: quality,
+        quality: quality.toString() + "p",
         duration: moment.duration(durationSec, "seconds").format("H [hour] mm [min]"),
         format: movie.split("_")[2].split(".")[1],
         filename: movie
@@ -45,4 +36,6 @@ app.get("/api/", async (req, res) => {
   res.send(movies)
 })
 
-const server = app.listen(3000)
+const server = app.listen(3000, () => {
+	console.log("Running on port 3000...");
+})
